f0f32752b0931fd660481a13fe8aaa062b111860
SirVer <jmonis@gmail.com>
2019-02-04 22:42:07 +0530

    - Implemented page image grabbing (as link) - Implemented search   - Search results are not currently read into a data structure     - The list is saved as Markdown instead     - This is better for the intended use-case, but may need to change - TODO: Stop using string manip for HTML construction, use Jsoup instead - TODO: Put pagequote and image in a table so they appear side-by-side - TODO: Implement argument parsing

--- src/main/kotlin/scraper.kt
+++ src/main/kotlin/scraper.kt
@@ -21,6 +21,9 @@ data class MainPage(val url: String) {
         }
     }
     private val article = document.selectFirst("div#main-article")
+    private val imageElement = document.select("div.quoteright")
+    private val image = if (imageElement.isNotEmpty()) imageElement.select("img")[0] else null
+    private val imageCaption = document.select("div.acaptionright")
     private val examplesHeader = article.select("h2:not(.comment-title)")
     private val mainText = article.select("#main-article > p,#main-article > h2,#main-article > ul")
     private val exampleFolderHeaders = article.select("div.folderlabel:not([onclick='toggleAllFolders();'])")
@@ -40,6 +43,8 @@ data class MainPage(val url: String) {
     var minimalHtml = ""
     init {
         minimalHtml += title.outerHtml()
+        minimalHtml += imageElement.outerHtml()
+        minimalHtml += imageCaption.outerHtml()
         minimalHtml += pageQuote.outerHtml()
         minimalHtml += pageQuoteSource.outerHtml()
         minimalHtml += mainText.outerHtml()
@@ -49,16 +54,10 @@ data class MainPage(val url: String) {
             minimalHtml += example.second
         }
     }
-    var pageJson = jsonObject(
-        "title" to title.toString(),
-        "pageQuote" to pageQuote.toString(),
-        "pageQuoteSource" to pageQuoteSource.toString(),
-        "mainText" to mainText.toString(),
-        "examplesHeader" to examplesHeader.toString(),
-        "examples" to jsonObject(examples)
-    )
     var pageTextJson = jsonObject(
         "title" to title.text(),
+        "imageElement" to imageElement.text(),
+        "imageCaption" to imageCaption.text(),
         "pageQuote" to pageQuote.text(),
         "pageQuoteSource" to pageQuoteSource.text(),
         "mainText" to jsonArray(mainText.eachText()),
@@ -76,16 +75,32 @@ data class MainPage(val url: String) {
         markdown = remark.convert(document.toString())
         minimalMarkdown = remark.convert(minimalHtml)
         markdownJson = jsonObject(
-            "title" to remark.convert(title.toString()),
-            "pageQuote" to remark.convert(pageQuote.toString()),
-            "pageQuoteSource" to remark.convert(pageQuoteSource.toString()),
-            "mainText" to remark.convert(mainText.toString()),
-            "examplesHeader" to remark.convert(examplesHeader.toString()),
+            "title" to remark.convert(title.outerHtml()),
+            "pageQuote" to remark.convert(pageQuote.outerHtml()),
+            "pageQuoteSource" to remark.convert(pageQuoteSource.outerHtml()),
+            "mainText" to remark.convert(mainText.outerHtml()),
+            "examplesHeader" to remark.convert(examplesHeader.outerHtml()),
             "examples" to jsonObject(examples.map { item -> Pair(item.first, remark.convert(item.second)) })
         )
     }
     override fun toString(): String {
         return "MainPage(title='${title.text()}', url='$url')"
+    }
+}
+
+data class SearchPage(val searchString: String) {
+    private val SEARCH_URL = "https://tvtropes.org/pmwiki/elastic_search_result.php"
+    private val parameters = hashMapOf<String, String>("q" to searchString, "page_type" to "all")
+    private val document = Jsoup.connect(SEARCH_URL).data(parameters).get()
+    private val results = document.select(".search-result")
+    val minimalHtml = results.outerHtml()
+    val minimalMarkdown: String
+    init {
+        val opts = Options.markdown()
+        opts.ignoredHtmlElements.add(IgnoredHtmlElement.create("spoiler"))
+        opts.inlineLinks = true
+        val remark = Remark(opts)
+        minimalMarkdown = remark.convert(minimalHtml)
     }
 }
 
@@ -97,8 +112,9 @@ fun main(args: Array<String>) {
     val page = MainPage(args[0])
     File("page.md").writeText(page.markdown)
     File("pageMin.md").writeText(page.minimalMarkdown)
-    File("page.json").writeText(page.pageJson.toString())
     File("pageText.json").writeText(page.pageTextJson.toString())
     File("markdown.json").writeText(page.markdownJson.toString())
     File("minimal.html").writeText(page.minimalHtml)
+    val searchPage = SearchPage("person of interest")
+    File("searchMarkdown.md").writeText(searchPage.minimalMarkdown)
 }
